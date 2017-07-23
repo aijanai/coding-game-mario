6328f0752596746c18ca243004fbb524f4193caf
Alberto Massidda <albertomassidda@aol.com>
2017-07-23 19:16:06 +0200

    First commit

---
+++ scriptEngine.js
@@ -0,0 +1,405 @@
+var initScriptEngine = function(){
+
+	//can you take a ladder?
+	window.atLadder=false;
+
+	//are you at the canyon
+	window.atCanyon=false;
+
+	//have you got the ladder
+	window.hasLadder=false;
+
+	//can you cross the canyon (because you put the ladder on it)
+	window.canyonTraversable=false;
+
+	//where the ladder is
+	window.ladderLocation=-3;
+
+	//where the canyon is
+	window.canyonLocation=11;
+
+	//where the hero is
+	window.heroLocation=0;
+
+	//the code generated
+	var plan='';
+
+	//flag for syntax checking during code generation
+	var inCondition=false;
+	var justClosedWhile=false;
+
+	//current execution step
+	var currentStep=1;
+
+	//list of actions for the B panel
+	var planList=new Array();
+
+	//call trace to move the viewPort
+	var callTrace= new Array();
+
+	//maximum execution steps allowed to prevent infinite loops
+	var maxSteps=50;
+
+	//victory flag, to fix final check
+	gameOver=false;
+
+	//container for current instruction
+	var scriptedAction=$('<div>');
+
+	//add visible element to script list
+	$('#scriptContainer').append(scriptedAction);
+
+	//printable actions map
+	var stringActions=new Array();
+	stringActions['takeLadder']= 'take the ladder';
+	stringActions['dropLadder']= 'drop the ladder';
+	stringActions['walkRight']= 'walk right';
+	stringActions['walkLeft']= 'walk left';
+	stringActions['AtLadder']= 'at the ladder, ';
+	stringActions['AtCanyon']= 'at the canyon, ';
+	stringActions['HasLadder']= 'have the ladder, ';
+	stringActions['while']= 'while ';
+	stringActions['if']= 'if ';
+	stringActions['not']= 'not ';
+
+	//reset everything
+	function resetGame(){
+    location.reload()
+	}
+
+	//terminates the game
+	function loseGame(){
+		message='after some time, the Princess gets tired and goes away: you lost';
+		gameLog(message);
+		gameOver=true;
+
+		//add to list function calls for the viewport (filter actions that have no graphical relevance)
+		callTrace.push('loseGame');
+	}
+
+	//checks whether the player won
+	function checkGameOutcome(){
+		//if you are past the canyon, announce victory
+		if((heroLocation - 2) > canyonLocation){
+			message='you got past the canyon: you won';
+			gameLog(message);
+			gameOver=true;
+
+			//add to list function calls for the viewport (filter actions that have no graphical relevance)
+			callTrace.push('winGame');
+		}
+
+		//if you reached maximum number of steps, terminate
+		if(currentStep >= maxSteps){
+			loseGame();
+		}
+	}
+
+	//increments game step, checks if maximum has been reached and takes action;
+	function incrementStepAndCheckExecutionTime(){
+		//increment step and check if maximum;
+		currentStep+=1;
+
+		//checks victory
+		checkGameOutcome();
+	}
+
+	//populate the output log
+	function gameLog(message){
+		//add to list of game events to be sent to B
+		planList.push(message);
+
+		//debug in panel A
+		console.log(message);
+	}
+
+	//check if you have a ladder
+	function HasLadder(){
+		if(hasLadder) gameLog('you got the ladder');
+		else gameLog('you have no ladder');
+
+		return hasLadder;
+	}
+
+
+	//check if you are not at the ladder
+	function AtLadder(){
+		if(atLadder) gameLog('you are at the ladder');
+		else gameLog('you are not yet at the ladder');
+
+		return atLadder;
+	}
+
+	//check if you are not at the canyon
+	function AtCanyon(){
+		if(atCanyon) gameLog('you are at the canyon');
+		else gameLog('you are not yet at the canyon');
+
+		return atCanyon;
+	}
+
+	//check if the player can reach ladder and canyon
+	function updateSurroundingStatus(){
+		atCanyon=heroLocation == canyonLocation;
+		atLadder=heroLocation == ladderLocation;
+	}
+
+	//move right
+	function walkRight(){
+		//count time
+		incrementStepAndCheckExecutionTime();
+
+		//check whether you can move past the canyon
+		if (atCanyon){
+			if(canyonTraversable){
+				//move past
+				heroLocation+=1;
+
+				//add to list function calls for the viewport
+				callTrace.push(arguments.callee.name);
+
+				//update log
+				gameLog('you walk over the canyon');
+
+				//winning status, perform check
+				checkGameOutcome();
+			}else{
+				//deny the action
+				gameLog('can\'t walk: there is a canyon');
+			}
+		}else{
+			//move right
+			heroLocation+=1;
+
+			//add to list function calls for the viewport
+			callTrace.push(arguments.callee.name);
+
+			//output log
+			gameLog('you walk right');
+		}
+
+		//if you have the ladder, the ladder moves with you
+		if(hasLadder){
+			//update ladder location
+			ladderLocation=heroLocation;
+		}
+
+		//check what you have at reach
+		updateSurroundingStatus();
+	}
+
+	//move left
+	function walkLeft(){
+		//count time
+		incrementStepAndCheckExecutionTime();
+
+		//move left
+		heroLocation-=1;
+
+		//add to list function calls for the viewport
+		callTrace.push(arguments.callee.name);
+
+		//if you have the ladder, the ladder moves with you
+		if(hasLadder){
+			//update ladder location
+			ladderLocation=heroLocation;
+		}
+
+		//output log
+		gameLog('you walk left');
+
+		//check what you have at reach
+		updateSurroundingStatus();
+	}
+
+	//take ladder, if you can
+	function takeLadder(){
+		//count time
+		incrementStepAndCheckExecutionTime();
+
+		//output log
+		gameLog('you try to take the ladder');
+
+		//check whether you can take the ladder
+		if(atLadder){
+			//take it
+			hasLadder=true;
+
+			//add to list function calls for the viewport
+			callTrace.push(arguments.callee.name);
+
+			//output log
+			gameLog('you got the ladder');
+		}else{
+			//deny the action
+			gameLog('there is no ladder to take');
+		}
+	}
+
+	//drop ladder, if you have it
+	function dropLadder(){
+		//count time
+		incrementStepAndCheckExecutionTime();
+
+		//check if you have it
+		if(hasLadder){
+			//drop it
+			hasLadder=false;
+
+			//add to list function calls for the viewport (filter actions that have no graphical relevance)
+			callTrace.push(arguments.callee.name);
+
+			//update ladder location
+			ladderLocation=heroLocation;
+
+		//special case: dropping it on the canyon makes it traversable
+			if(atCanyon){
+				//update world status
+				canyonTraversable=true;
+
+				//output special log
+				gameLog('you drop the ladder over the canyon');
+			}else{
+				//output log
+				gameLog('you drop the ladder');
+			}
+		}else{
+			//deny action
+			gameLog('you didn\' have any ladder to drop');
+		}
+
+		//check what you have at reach
+		updateSurroundingStatus();
+	}
+
+	//execute script
+	$('#play').on('click',function(){
+
+		//if the flag is still raised, the player left a while without subject action; stop
+		if(justClosedWhile){
+			$('#modalErrorNoInstruction').openModal();
+			return;
+		}
+
+		//execute generated code
+		eval(plan);
+		console.log(plan);
+
+		//if plan was not sufficient to carry out goal, kill the player
+		if(!gameOver) loseGame();
+		console.log(planList);
+		console.log(callTrace);
+
+    viewportFunctions.playAnimation(callTrace);
+	});
+
+	//reset game
+	$('#reset').on('click',function(){
+		//clear plan
+		$('#scriptContainer').html('');
+
+		//reset game status
+		resetGame();
+	});
+
+	//wheneven the player clicks on an action
+	$('#actionsContainer').on('click','.line',function(){
+		//get action name
+		var instruction=$(this).attr("id");
+
+
+		//while has a strict syntax; if while is selected
+		if(('while'==instruction || 'if'==instruction) && !inCondition){
+
+			//take note and demand a condition afterwards
+			inCondition=true;
+
+			//select right behaviour for conditional instructions
+			switch(instruction) {
+				case 'while':
+					//generate code for open while (with infinite loop avoidance condition)
+						plan=plan.concat('while(currentStep<'+maxSteps+' && ');
+
+					//generate output in the current container
+					scriptedAction.append($('<span>').text(stringActions[instruction]));
+					break;
+				case 'if':
+					//generate code for open if
+					plan=plan.concat('if( ');
+
+					//generate output in the current container
+					scriptedAction.append($('<span>').text(stringActions[instruction]));
+					break;
+			}
+		}else{
+			//if it's another action
+			//if in while, demand a condition
+			if(inCondition){
+				//if not a condition
+				if(!$(this).hasClass('condition')){
+
+					//demand it
+					$('#modalErrorNoCondition').openModal();
+
+					//and refuse to add the action
+					return;
+				}else{
+
+					//if it's a negation
+					if('not'==instruction){
+
+						//generate code for not
+						plan=plan.concat('!');
+
+						//generate output in the current container
+						scriptedAction.append($('<span>').text(stringActions[instruction]));
+
+					}else{
+						//add the condition with closing while parenthese
+						plan=plan.concat(instruction+'()) ');
+
+						//generate output in the current container
+						scriptedAction.append($('<span>').text(stringActions[instruction]));
+
+						//close the while after action is added
+						inCondition=false;
+
+						//raise flag for syntax check of while termination block
+						justClosedWhile=true;
+					}
+				}
+			}else{
+
+				//prevent putting a boolean or negation without being in the need of it
+				switch(instruction) {
+					case 'not':
+					case 'AtCanyon':
+					case 'AtLadder':
+					case 'HasLadder':
+						//demand it
+						$('#modalErrorConditionNegation').openModal();
+
+						//and refuse to add the action
+						return;
+				}
+				//add the action
+				plan=plan.concat(instruction+'(); ');
+
+				//generate output in the current container
+				scriptedAction.append($('<span>').text(stringActions[instruction]));
+
+				//create a new visible element container
+				scriptedAction=$('<div>');
+
+				//add visible element to script list
+				$('#scriptContainer').append(scriptedAction);
+
+				//switch off the flag so we know the syntax is good
+				if(justClosedWhile) justClosedWhile=false;
+			}
+		}
+
+	});
+};
+
+window.initScriptEngine = initScriptEngine
---
+++ viewportEngine.js
@@ -0,0 +1,146 @@
+var viewportFunctions = {};
+
+  window.viewportAtCanyon=false;
+  window.viewportHasLadder=false;
+  window.viewportCanyonTraversable=false;
+  window.viewportLadderLocation=-3;
+  window.viewportCanyonLocation=3;
+  window.viewportHeroLocation=0;
+
+var initViewportEngine = function(){
+
+  viewportFunctions.walkRight = function(callbacks){
+    viewportFunctions.flip(1);
+    $('#sprite-image').attr("src", "images/mario_move.gif");
+    if ((!viewportCanyonTraversable && parseInt($('#sprite').css("margin-left")) < 460) || (viewportCanyonTraversable && parseInt($('#sprite').css("margin-left")) < 550)){
+      move('#sprite')
+        .duration(1000)
+        .add('margin-left', 30)
+        .end(function(){
+          $('#sprite-image').attr("src", "images/mario_stop.png");
+          if (callbacks.length > 0) {
+            console.log(callbacks);
+            var functionToCall = callbacks.shift();
+            eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+          }
+        });
+    } else {
+      setTimeout(function(){
+        $('#sprite-image').attr("src", "images/mario_stop.png");
+        if (callbacks.length > 0) {
+          console.log(callbacks);
+          var functionToCall = callbacks.shift();
+          eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+        }
+      }, 800);
+    };
+  };
+
+
+
+  viewportFunctions.walkLeft = function(callbacks){
+    viewportFunctions.flip(-1);
+    $('#sprite-image').attr("src", "images/mario_move.gif");
+    if (parseInt($('#sprite').css("margin-left")) > 70) {
+      move('#sprite')
+        .duration(1000)
+        .sub('margin-left', 30)
+        .end(function(){
+          $('#sprite-image').attr("src", "images/mario_stop.png");
+          if (callbacks.length > 0) {
+            console.log(callbacks);
+            var functionToCall = callbacks.shift();
+            eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+          }
+        });
+    } else {
+      setTimeout(function(){
+        $('#sprite-image').attr("src", "images/mario_stop.png");
+        if (callbacks.length > 0) {
+          console.log(callbacks);
+          var functionToCall = callbacks.shift();
+          eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+        }
+      }, 800);
+    }
+  };
+
+
+  viewportFunctions.takeLadder = function(callbacks){
+    if (viewportFunctions.viewportAtLadder()) {
+      $('#ladder-sprite').hide();
+      viewportHasLadder = true;
+    }
+    if (callbacks.length > 0) {
+      console.log(callbacks);
+      var functionToCall = callbacks.shift();
+      eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+    }
+  }
+
+  viewportFunctions.dropLadder = function(callbacks){
+    if (viewportHasLadder) {
+      viewportHasLadder = false;
+      $('#ladder-sprite').css("padding-left", $("#sprite").css("margin-left"));
+      $('#ladder-sprite').show();
+      if (viewportFunctions.viewportAtCanyon()){
+        move("#ladder-sprite-image").add("margin-left", 35).end();
+        move("#ladder-sprite-image").rotate(90).end();
+        move("#ladder-sprite-image").add("margin-top", 25).end();
+        viewportCanyonTraversable = true;
+      }
+    }
+    if (callbacks.length > 0) {
+      console.log(callbacks);
+      var functionToCall = callbacks.shift();
+      eval(functionToCall + '([' + callbacks.map(function(o){return '"' + o + '"'}) + '])');
+    }
+  }
+
+  viewportFunctions.flip = function(direction){
+    move("#sprite-image")
+      .set('-moz-transform', 'scaleX(' + direction + ')')
+      .set('-o-transform', 'scaleX(' + direction + ')')
+      .set('-webkit-transform', 'scaleX(' + direction + ')')
+      .set('transform', 'scaleX(' + direction + ')')
+      .end()
+  };
+
+  viewportFunctions.playAnimation = function(callTrace){
+    var functionsStack = callTrace.map(function(o){return "viewportFunctions." + o})
+    var functionToCall = functionsStack.shift()
+    eval(functionToCall + '([' + functionsStack.map(function(o){return '"' + o + '"'}) + '])');
+  }
+
+  viewportFunctions.loseGame = function(callTrace){
+				$('#scriptContainer').append("After some time, the Princess gets tired and goes away you: you lost");
+  }
+
+  viewportFunctions.winGame = function(callTrace){
+				$('#scriptContainer').append("Congrats, you did it");
+  }
+
+  viewportFunctions.viewportAtLadder = function(){
+    var marioPL = parseInt($('#sprite').css("margin-left"));
+    var ladderPL = parseInt($('#ladder-sprite').css("padding-left"));
+    return (Math.abs((marioPL - ladderPL)) < 16)
+  }
+
+
+  viewportFunctions.viewportAtCanyon = function(){
+    var marioPL = parseInt($('#sprite').css("margin-left"));
+    return (Math.abs((marioPL - 480)) < 25)
+  }
+
+
+  window.viewportFunctions = viewportFunctions;
+
+
+  // initial move to put mario right enough
+  setTimeout(function(){
+    viewportFunctions.walkRight(["viewportFunctions.walkRight", "viewportFunctions.walkRight", "viewportFunctions.walkRight"]);
+  }, 400);
+
+};
+
+window.initViewportEngine = initViewportEngine;
